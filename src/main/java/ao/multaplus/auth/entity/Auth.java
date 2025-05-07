0646df5bae461fc23d09cdaeef582db376e438ec
Marcelino <mfigueira1901@gmail.com>
2025-05-07 19:58:43 +0100

    ...

--- src/main/java/ao/multaplus/auth/entity/Auth.java
+++ src/main/java/ao/multaplus/auth/entity/Auth.java
@@ -49,7 +49,7 @@ public class Auth extends AbstractModel implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if(false) {
+        if(role.getRole().equalsIgnoreCase("admin")) {
             return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
         }
         else {
